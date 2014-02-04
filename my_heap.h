a6bfcf84b2dfe3d02d0fae463a0469ded417f6c6
Ya-Lin Huang <yalinh@yelp.com>
2014-02-03 16:37:19 -0800

    fix comments

--- my_heap.h
+++ my_heap.h
@@ -4,13 +4,36 @@
 #include <algorithm>
 #include <functional>
 
+/* replace_heap() and pushpop_heap() in this file invovles optimization tricks
+ * for saving runtime.
+ *
+ * For example, replace_heap() is equivalent to pop_heap() then push_heap(),
+ * which takes two O(log N):
+ * One for pop_heap():
+ *	1. replace head of the heap with the last element
+ *	2. heapify from the top
+ * And the other for push_heap():
+ *	1. given the new data is the last element, heapify from the bottom
+ *
+ * In fact, replace_heap() can be done as follows using one O(log N):
+ * 1. replace head of the heap with the new data
+ * 2. heapify from the top
+ *
+ * Similar trick applies to pushpop_heap().
+ *
+ * This implementation is motivated by Python's
+ * heapq.heapreplace() and heapq.heappushpop().
+ */
+
+
+
 
 /* replace_heap(): pop_heap() first, then push_heap()
  *
  * The original heap (excluding `data`) must be at from `first` to `last-2`,
  * and `last-1` must be available for temporary use.
  *
- * Heapification is based on `comp`. In generatl,
+ * Heapification is based on `comp`. In general,
  * std::less<>() for max-heap and std::greater<>() for min-heap.
  */
 template <class RandomAccessIterator, class T, class Compare>
